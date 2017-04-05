0ec75830c2bc03f17746a20e3446e125739bae69
Guillaume Ballet <gballet@gmail.com>
2017-04-05 18:34:50 -0400

    Unit test configuration

---
+++ karma.conf.js
@@ -0,0 +1,40 @@
+// Karma configuration
+var webpackConfig = require("./webpack.config.js");
+
+module.exports = function(config) {
+  config.set({
+    basePath: './',
+    frameworks: ["mocha"],
+
+    // ... normal karma configuration
+    files: [
+      // all files ending in "-test"
+      {pattern: 'test/*-test.js', watched: false},
+      {pattern: 'test/**/*-test.js', watched: false}
+      // each file acts as entry point for the webpack configuration
+    ],
+
+    preprocessors: {
+      // add webpack as preprocessor
+      'test/*-test.js': ['webpack'],
+      'test/**/*-test.js': ['webpack']
+    },
+
+    webpack: webpackConfig,
+
+    webpackMiddleware: {
+      // webpack-dev-middleware configuration
+      // i. e.
+      noInfo: true
+    },
+
+    plugins: [
+      require("karma-webpack"),
+      require("karma-mocha"),
+      require("karma-sinon-chai"),
+      require("karma-phantomjs-launcher")
+    ],
+
+    browsers: ["PhantomJS"]
+  });
+};
