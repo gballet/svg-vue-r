9b698e181dcf79abe6cdb1a9b7d7298c715ef079
Brandon Shiluk <bshiluk@gmail.com>
2017-07-28 11:57:42 -0700

    Updated markup for Text Editor
    Now positioned absolutely within svg box with veritcal positioning relative to selected text el

--- src/TextEditor.vue
+++ src/TextEditor.vue
@@ -1,10 +1,20 @@
 <template>
-    <div>
-        Input text:
-        <input v-model="value.text" />
-        <input v-model.number="value.size" type="number" min="1" class="font-size" />
-        <button @click="save">Save</button>
-        <button @click="cancel">Cancel</button>
+    <div class="text-editor" :style="{top: value.y + 24 + 'px'}"> 
+        <label class="edited-text">
+            <textarea rows="4" placeholder="Please enter the text" v-model="value.text"></textarea>
+        </label>
+        <div class="text-editor-options">
+            <div>
+                <button @click="save">Save</button>
+                <button @click="cancel">Cancel</button>
+            </div>
+            <div>
+                <label class="font-size">
+                    Font Size:
+                    <input type="number" min="1" v-model.number="value.size">
+                </label>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -31,7 +41,71 @@ export default {
 </script>
 
 <style>
-.font-size {
-    width: 3em;
+.text-editor{
+    position:absolute;
+    top:40%;
+    left:0;
+    right:0;
+    margin:0 auto;
+    width:62%;
+    background:#fff;
+    box-shadow:0 2px 8px rgba(0,0,0,.1);
+    padding:12px;
+    border:1px solid #f8f8f8;
+    border-radius:2px;
+    z-index:1;
 }
+label.edited-text{
+    display:block;
+    overflow:hidden;
+    position:relative;
+}
+label.edited-text > textarea{
+    width:100%;
+    background-color:#fafafa;
+    resize:none;
+    padding:8px;
+    border:none;
+    box-sizing:border-box;
+    margin:0;
+}
+.text-editor-options{
+    position:relative;
+    overflow:hidden;
+    height:40px;
+}
+.text-editor-options > div{
+    float:left;
+    height:inherit;
+}
+.text-editor-options > div:first-of-type{
+    width:62%;
+}
+.text-editor-options > div:first-of-type > button{
+    margin-top:12px;
+}
+.text-editor-options > div:last-of-type{
+    width:38%;
+    text-align:right;
+}
+label.font-size{
+    display:inline-block;
+    width:50px;
+    text-align:left;
+    margin:0;
+    font-size:10px;
+    font-weight:bold;
+    font-family:monospace;
+    word-spacing: -2px;
+    letter-spacing: -1px;
+}
+label.font-size > input{
+    width:100%;
+    padding:0;
+    margin:2px 0 0 0;
+    box-sizing:border-box;
+}
+
+
+
 </style>
