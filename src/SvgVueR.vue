0714817c9a61460ed8f294ab77cd0063ff3e7319
Brandon Shiluk <bshiluk@gmail.com>
2017-08-20 13:44:48 -0700

    adjust z-index and height/width of resizer

--- src/SvgVueR.vue
+++ src/SvgVueR.vue
@@ -38,9 +38,7 @@
             </rect>
         </svg>
         <div v-if="resizing" class="resizer" :style="{top:height + 'px', left:width + 'px'}"
-            @mousemove.stop="resize"
-            @mouseup.stop="resizeEnd"
-            @mouseleave="resizeEnd">
+            @mousemove.stop="resize" @mouseleave.stop="resizeEnd" @mouseup.stop="resizeEnd" >
         </div>
         <div class="toolbar">
             <button @click.prevent="setTool(_tool.name)" v-for="(_tool, index) in tools" :key="index" :class="tool == _tool.name ? 'tool active' : 'tool inactive'">
@@ -274,9 +272,10 @@ export default {
         display:inline-block;
     }
     .resizer{
-        position:absolute;
-        height:200px;
-        width:200px;
+        position:fixed;
+        z-index:10;
+        height:100vh;
+        width:100vw;
         background:transparent;
         -webkit-transform:translate3d(-50%,-50%,0);
         transform:translate3d(-50%,-50%,0);
